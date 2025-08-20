1f8d2d4312185109eda730b1449e2486c80f97b9
Jason Forlinx <32384517+HelloTangle@users.noreply.github.com>
2025-08-20 15:10:50 +0800

    update

--- platform/allwinner/ok527n-c/conf.py
+++ platform/allwinner/ok527n-c/conf.py
@@ -13,7 +13,9 @@ copyright = 'Forlinx Embedded'
 
 extensions = [
     'myst_parser',         # 支持 Markdown
+    'sphinx_sitemap',      # 添加 sitemap 扩展
 ]
+html_baseurl = "https://hellotangle.github.io/forlinx-docs-HT/allwinner/ok527n-c/"
 
 
 
