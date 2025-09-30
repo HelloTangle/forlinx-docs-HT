126610774fe433a60e3f9fdeee0058744f9b57c3
Jason Forlinx <32384517+HelloTangle@users.noreply.github.com>
2025-09-30 17:32:33 +0800

    212212

--- platform/rockchip/ok3576-c/_static/custom.js
+++ platform/rockchip/ok3576-c/_static/custom.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', function() {
     // ---------------------------
     // 1) Logo 强制跳转（兜底）
@@ -13,24 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // ---------------------------
     // 2) 左侧目录折叠逻辑
     // ---------------------------
-    setTimeout(function() {
-        var lis = document.querySelectorAll('.wy-menu-vertical li.toctree-l1, .wy-menu-vertical li.toctree-l2');
-    
-        // 克隆节点，去掉主题原事件绑定
-        lis.forEach(function(li) {
-            var newLi = li.cloneNode(true);
-            li.parentNode.replaceChild(newLi, li);
-        });
-    
-        // 重新绑定点击事件
-        var toctreeLinks = document.querySelectorAll('.wy-menu-vertical li.toctree-l1 > a, .wy-menu-vertical li.toctree-l2 > a');
-        toctreeLinks.forEach(function(link) {
-            link.addEventListener('click', function(e){
-                var parentLi = link.parentElement;
-                parentLi.classList.toggle('current'); // 切换当前目录展开/折叠
-                e.stopPropagation(); // 阻止冒泡
-            });
-        });
-    }, 500);
+    document.querySelectorAll('.wy-menu-vertical li.toctree-l1, .wy-menu-vertical li.toctree-l2').forEach(function(li){
+        li.classList.add('current'); // 强制全部展开
+    });
     
 });
